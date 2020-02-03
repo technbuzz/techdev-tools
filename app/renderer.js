1a220306acff9f8abad4f4322dfc32adb46d895d
Sami Ullah <sami.ullah@cadt-solutions.com>
2020-02-03 14:54:47 +0400

    escape html added

--- app/renderer.js
+++ app/renderer.js
@@ -1,7 +1,8 @@
-const marked = require('marked');
+const escape = require('escape-html');
 
 const markdownView = document.querySelector('#markdown');
-const htmlView = document.querySelector('#html');
+// const htmlView = document.querySelector('#html');
+const escapedHtml = document.querySelector('#escaped-html');
 const newFileButton = document.querySelector('#new-file');
 const openFileButton = document.querySelector('#open-file');
 const saveMarkdownButton = document.querySelector('#save-markdown');
@@ -10,8 +11,10 @@ const saveHtmlButton = document.querySelector('#save-html');
 const showFileButton = document.querySelector('#show-file');
 const openInDefaultButton = document.querySelector('#open-in-default');
 
-const renderMarkdownToHtml = markdown => {
-  htmlView.innerHTML = marked(markdown, { sanitize: true });
+const renderMarkdownToHtml = markup => {
+  
+  escapedHtml.value = escape(markup);
+  console.log('escapedhtml', escape(markup));
 };
 
 markdownView.addEventListener('keyup', event => {
