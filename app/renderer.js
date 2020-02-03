296f722e60196f9fa5a8983273f1855556c86431
Sami Ullah <sami.ullah@cadt-solutions.com>
2020-02-03 15:08:53 +0400

    added clipboard functionality

--- app/renderer.js
+++ app/renderer.js
@@ -1,23 +1,32 @@
 const escape = require('escape-html');
+const { clipboard } = require('electron')
+
+let escapedHTML = null;
+
 
 const markdownView = document.querySelector('#markdown');
 // const htmlView = document.querySelector('#html');
-const escapedHtml = document.querySelector('#escaped-html');
+const escapedHtmlButton = document.querySelector('#escaped-html');
 const newFileButton = document.querySelector('#new-file');
 const openFileButton = document.querySelector('#open-file');
 const saveMarkdownButton = document.querySelector('#save-markdown');
 const revertButton = document.querySelector('#revert');
 const saveHtmlButton = document.querySelector('#save-html');
-const showFileButton = document.querySelector('#show-file');
-const openInDefaultButton = document.querySelector('#open-in-default');
+const copyEscapedHTMLButton = document.querySelector('#copy-src');
 
 const renderMarkdownToHtml = markup => {
   
-  escapedHtml.value = escape(markup);
-  console.log('escapedhtml', escape(markup));
+  escapedHtmlButton.value = escape(markup);
+  return escape(markup)
 };
 
 markdownView.addEventListener('keyup', event => {
   const currentContent = event.target.value;
-  renderMarkdownToHtml(currentContent);
+  escapedHTML =  renderMarkdownToHtml(currentContent);
 });
+
+copyEscapedHTMLButton.addEventListener('click', () => {
+  console.log(escapedHTML);
+  clipboard.writeText(escapedHTML)
+})
+
